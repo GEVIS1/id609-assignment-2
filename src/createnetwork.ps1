0c3b331cb083ebc5bcc6088d3db8fb41f4922862
Steffen Geving <gevis1@student.op.ac.nz>
2022-10-17 11:57:11 +1300

    feat: add progress to createnetwork

--- src/createnetwork.ps1
+++ src/createnetwork.ps1
@@ -1,7 +1,10 @@
 # Create the virtual network
 $virtualnetwork = New-AzVirtualNetwork -ResourceGroupName $ResourceGroupName -Location $Location -Name $VirtualNetworkName -AddressPrefix 192.168.0.0/16
 
+
+#TODO: Fix
+#https://learn.microsoft.com/en-us/powershell/module/az.network/new-azvirtualnetworksubnetconfig?view=azps-9.0.0
 # Create the subnets
-Add-AzVirtualNetworkSubnetConfig -VirtualNetwork $virtualnetwork -Name "subnet1" -AddressPrefix 192.168.1.0/24
-Add-AzVirtualNetworkSubnetConfig -VirtualNetwork $virtualnetwork -Name "subnet2" -AddressPrefix 192.168.2.0/24
-Add-AzVirtualNetworkSubnetConfig -VirtualNetwork $virtualnetwork -Name "subnet3" -AddressPrefix 192.168.3.0/24
+$subnet1 = Add-AzVirtualNetworkSubnetConfig -VirtualNetwork $virtualnetwork -Name "subnet1" -AddressPrefix 192.168.1.0/24
+$subnet2 = Add-AzVirtualNetworkSubnetConfig -VirtualNetwork $virtualnetwork -Name "subnet2" -AddressPrefix 192.168.2.0/24
+$subnet3 = Add-AzVirtualNetworkSubnetConfig -VirtualNetwork $virtualnetwork -Name "subnet3" -AddressPrefix 192.168.3.0/24
