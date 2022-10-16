b573d0672a734ecee05a567017aa9877b80fc13c
Steffen Geving <gevis1@student.op.ac.nz>
2022-10-17 11:56:26 +1300

    feat: add progress to createvms

--- src/createvms.ps1
+++ src/createvms.ps1
@@ -1,39 +1,38 @@
 # Get the login credentials for the VMs. Ideally this would be three separate credentials, but it is one here for convenience.
-$cred = Get-Credential -Message "Input credentials for VMS"
+#$cred = Get-Credential -Message "Input credentials for VMS"
 
-# VM1 config
-$vm1config = @{
-ResourceGroupName="ID609OE1-gevis1"
-Location="australiaeast"
-Name="steffenc911"
-Image="win2019Datacenter" 
-Size="Standard_B2s"
-PublicIpAddressName="steffen-publicip1"
-VirtualNetworkName="steffen-vnet"
-SubnetName="steffen-subnet"
-OpenPorts=3389,22
-Credential=$cred
-}
+# Get the vnet
+$vnet = $(Get-AzVirtualNetwork -ResourceGroupName $ResourceGroupName -Name $VirtualNetworkName)
+Write-Host "Writing subnet names from `$vnet"
+Write-Host $vnet.Subnets[0].Name
+Write-Host $vnet.Subnets[1].Name
+Write-Host $vnet.Subnets[2].Name
+Write-Host $vnet.Subnets[3].Name
+Write-Host "Writing subnet names from `$subnet#"
+Write-Host $subnet1
+Write-Host $subnet2
+Write-Host $subnet3
 
-# VM2 config
-$vm2config = @{
-ResourceGroupName="ID609OE1-gevis1"
-Location="australiaeast"
-Name="steffenc912"
-Image="win2019Datacenter" 
-Size="Standard_B2s"
-PublicIpAddressName="steffen-publicip2"
-VirtualNetworkName="steffen-vnet"
-SubnetName="steffen-subnet"
-OpenPorts=3389,22
-Credential=$cred
-}
+# Server A config
+#$nic1 = New-AzNetworkInterface -Name ServerANIC -ResourceGroupName $ResourceGroupName -Location $Location -SubnetId $vnet.Subnets[0].Id
 
-# Create the VMs
-$vm1 = New-AzVM @vm1config
-$vm2 = New-AzVM @vm2config
+# $vm1config = @{
+# ResourceGroupName=$ResourceGroupName
+# Location=$Location
+# Name="steffenc911"
+# Image="win2019Datacenter" 
+# Size="Standard_B2s"
+# VirtualNetworkName="steffen-vnet"
+# SubnetName="steffen-subnet"
+# OpenPorts=3389,22
+# Credential=$cred
+# }
 
-# Connect to the VMs with RDP
-Get-AzRemoteDesktopFile -ResourceGroupName $vm1.ResourceGroupName -Name $vm1.Name -Launch
-Get-AzRemoteDesktopFile -ResourceGroupName $vm2.ResourceGroupName -Name $vm2.Name -Launch
+# # Create the VMs
+# $vm1 = New-AzVM @vm1config
+# $vm2 = New-AzVM @vm2config
+
+# # Connect to the VMs with RDP
+# Get-AzRemoteDesktopFile -ResourceGroupName $ResourceGroupName -Name $vm1.Name -Launch
+# Get-AzRemoteDesktopFile -ResourceGroupName $ResourceGroupName -Name $vm2.Name -Launch
 
